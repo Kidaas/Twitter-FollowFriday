e0d7edddde43dd7e92b058c0f000ab66e6b21300
Kevin SARRAZIN <sarrazin.kevin@live.fr>
2015-07-29 15:49:01 +0200

    [Ajout] Système de cache + changement type requete

--- config/config.php
+++ config/config.php
@@ -1,11 +1,18 @@
 <?php
 
-    // Nombre de listes de friends désiré, 1 = 20 friends, 2 = 40 friends etc...
-    $max = 1;
+    // Nombre de listes de friends désiré
+    $count = 40;
+
+    // Pseudo de l'utilisateur dont on veut les abonnements.
+    $pseudo = 'kidaas';
 	
+    // Fichier de cache des tweets
+    $cache = 'cache/follow.tmp';
+
 	// Informations de votre compte twitter dev : https://apps.twitter.com/
     $consumer_key=''; //Application consumer key
     $consumer_secret=''; //Application consumer secret key
     $oauth_token = ''; //oAuth Token
     $oauth_token_secret = ''; //oAuth Token Secret
+
 	
--- index.php
+++ index.php
@@ -3,74 +3,68 @@
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width,initial-scale=1"/>
-	<title>Afficher derniers friends twitter</title>
+	<meta name="description" content="Application FollowFriday en PHP. Disponible sur mon Github" />
+	<title>Follow Friday</title>
 	<link rel="stylesheet" type="text/css" media="all" href="style.css" />
 </head>
 
-<body class="page lang-fr">
-	<header>
-		<a class="brand" href="http://kevinsarrazin.fr/"><img src="./img/user.jpg" width="50px" /></a>
-	</header>
+<body>
 
-	<aside id="reseaux">
-		<a href="https://twitter.com/Kidaas" class="twitter-follow-button" data-show-count="false">Follow @Kidaas</a>
-		<a href="https://twitter.com/share" class="twitter-share-button" data-via="Kidaas" data-hashtags="FollowFriday">Tweet</a>
-		<a href="https://github.com/Kidaas" target="_Blank"><img src="./img/github.gif" width="100px"/></a>
-	</aside>
+	<nav id="reseaux">
+		<ul>
+			<li><a href="https://twitter.com/Kidaas" target="_blank">- Follow @Kidaas -</a></li>
+			<li><a href="https://twitter.com/share" target="_blank">- Tweeter -</a></li>
+			<li><a href="https://github.com/Kidaas" target="_blank">- Github -</a></li>
+		</ul>
+	</nav>
 	
 	<section>
 		<div class="friends" >
-	<?php 
-		// Paramètres de configuration
-		include('./config/config.php');
-		//La bibliothèque oAuth
-		require_once ('./twitteroauth/twitteroauth.php'); 
-		
-		//Création de l'objet
-		$connection = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret);
-		$connection->host = "https://api.twitter.com/1.1/";
-		
-		$i = 0;
-		// -1 = Liste des derniers friends en date
-		$cursor = -1;
-		
-		While ($i < $max && $cursor != 0){
-			// Requète
-			$query = 'https://api.twitter.com/1.1/friends/list.json?cursor='.$cursor.'&screen_name=xhark';
-			$content = $connection->get($query);
-			
-			// Test si $content n'est pas vide et ne contient pas d'erreur
-			if(!empty($content) && empty($content->errors[0]->code)){ 
-				// Récupère le curseur permettant d'atteindre les 20 prochains enregistrements
-				$cursor = $content->next_cursor_str;
-				foreach($content as $list){
-					if(is_array($list)){ 
-						foreach($list as $user){
-							if(!empty($user)){ 		
-								echo '<a class="popup" href="https://twitter.com/'.$user->screen_name.'" target="_Blank">
-										<img src= "'.$user->profile_image_url.'" />
-										<span>
-											<p>Pseudo : '.$user->screen_name.'</p>
-											<p>Nom : '.$user->name.'</p>
-										</span>
-									  </a>';		 
-							}
-						}
-					}
+			<?php 
+				// Paramètres de configuration
+				include('./config/config.php');
+				//La bibliothèque oAuth
+				require_once ('./twitteroauth/twitteroauth.php'); 
+				
+				$new = false;
+
+				// Test si le fichier temporaire existe
+				// Si il n'existe pas, le créer
+				if (!file_exists($cache)){ 
+					fclose(fopen($cache, 'w'));
+					$new = true;
 				}
-			}else {
-				echo '<span class="erreur"><img src="./img/warning.png" width="30px" /> Erreur limite de requète sur la base de données de twitter atteinte. (Attente de 15min maximum).</span>';
-				break;
-			}
-			$i++;
-		}
-	?>
-	</div>
+
+				if ($new || time() - filemtime($cache) > 600){
+						//Création de l'objet
+						$connection = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret);
+						$connection->host = "https://api.twitter.com/1.1/";
+						// Requete
+						$content = $connection->get('friends/list', array('count' => $count, 'screen_name' => $pseudo));
+						// Met en cache
+						file_put_contents($cache, serialize($content));
+
+				}else{ // Récupère les données en cache
+					$content = unserialize(file_get_contents($cache));
+				}
+
+				// Parcour les tweets et les affiches
+				foreach ($content->users as $key => $user){
+					echo '<a class="popup" href="https://twitter.com/'.$user->screen_name.'" target="_blank">
+							<img src= "'.$user->profile_image_url.'" />
+							<span>
+								<strong>'.$user->name.'</strong>
+								<p class="pseudo">@'.$user->screen_name.'</p>
+								<p class="description">'.$user->description.'</p>
+							</span>
+						  </a>';	
+				}
+			?>
+		</div>
 	</section>
 	<footer>
-		<a href="http://kevinsarrazin.fr/">© 2014 Kidas.</a>		
+		<a href="http://kevinsarrazin.fr/" target="_blank">© 2014 Kidas.</a>		
 	</footer>
 	
-	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>	
 	</body>
 </html>
